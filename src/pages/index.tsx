04f00770dfad9e574ced05932a89867d9215cfae
jamesworden <jamesworden.ny@gmail.com>
2023-07-30 10:42:51 -0400

    More things

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,10 +7,28 @@ import headshot from '../images/headshot.jpg';
 const IndexPage: React.FC<PageProps> = () => {
 	return (
 		<main className={indexStyles.container}>
-			<div className={indexStyles.headshotContainer}>
-				<img alt='Headshot' src={headshot} className={indexStyles.headshot} />
-				<div className={indexStyles.roundedBackground} />
-			</div>
+			<section className={indexStyles.menuSection}>
+				<span>James Worden</span>
+				<button>Contact</button>
+			</section>
+
+			<section className={indexStyles.heroSection}>
+				<div className={indexStyles.heroDescriptionContainer}>
+					<div className={indexStyles.heroDescription}>
+						<h1>I'm James Worden.</h1>
+						<span>I strive to build simple and</span>
+						<br />
+						<span>intuitive software.</span>
+					</div>
+
+					<button>See My Work</button>
+				</div>
+
+				<div className={indexStyles.headshotContainer}>
+					<img alt='Headshot' src={headshot} className={indexStyles.headshot} />
+					<div className={indexStyles.roundedBackground} />
+				</div>
+			</section>
 		</main>
 	);
 };
