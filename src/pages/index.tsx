62544681c43206a2cc55bb9670d1be5e192226c4
jamesworden <jamesworden.ny@gmail.com>
2023-07-30 00:01:28 -0400

    Got a headshot with maroon bg

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,17 @@ import * as React from 'react';
 import type { HeadFC, PageProps } from 'gatsby';
 
 import * as indexStyles from '../styles/index.module.scss';
+import headshot from '../images/headshot.jpg';
 
 const IndexPage: React.FC<PageProps> = () => {
-	return <main className={indexStyles.container}>Test</main>;
+	return (
+		<main className={indexStyles.container}>
+			<div className={indexStyles.headshotContainer}>
+				<img alt='Headshot' src={headshot} className={indexStyles.headshot} />
+				<div className={indexStyles.roundedBackground} />
+			</div>
+		</main>
+	);
 };
 
 export default IndexPage;
