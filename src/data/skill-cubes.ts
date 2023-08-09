824ddf0d9b909473a3f0eb0cf77892aad73a673a
jamesworden <jamesworden.ny@gmail.com>
2023-08-09 10:08:07 -0400

    skill cube things

--- src/data/skill-cubes.ts
+++ src/data/skill-cubes.ts
@@ -7,22 +7,22 @@ import reactLogo from '../images/react-logo.png';
 export const skillCubes: ISkillCube[] = [
 	{
 		rotation: [1.2, 1, 1],
-		position: [-2, 1, -2],
+		position: [-2, 10, -2],
 		image: javascriptLogo,
 	},
 	{
 		rotation: [1.2, 1.1, 1],
-		position: [2, 1, -3],
+		position: [2, 10, -3],
 		image: typescriptLogo,
 	},
 	{
 		rotation: [1, 0.9, 1.2],
-		position: [0, 1, -1],
+		position: [0, 10, -1],
 		image: angularLogo,
 	},
 	{
 		rotation: [1, 0.9, 1.2],
-		position: [2, 1, -2],
+		position: [2, 5, -2],
 		image: reactLogo,
 	},
 ];
