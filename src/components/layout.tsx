db3ed66999b6d104ee7904400a4f5cc6a686546f
jamesworden <jamesworden.ny@gmail.com>
2023-08-30 16:06:52 -0400

    Realized that overflow hidden prevents sticky nav from working

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -16,7 +16,7 @@ export const Layout: React.FC<LayoutProps> = ({
 	onAnimationComplete,
 }) => {
 	return (
-		<main className='bg-gray-100 dark:bg-gray-900 transition dark:text-gray-100 h-full min-h-screen justify-between flex flex-col overflow-hidden'>
+		<main className='bg-gray-100 dark:bg-gray-900 transition dark:text-gray-100 h-full min-h-screen justify-between flex flex-col'>
 			<Navbar contactSectionId={contactSectionId}></Navbar>
 
 			<motion.main
