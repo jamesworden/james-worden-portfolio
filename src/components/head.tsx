3c7f64d72fd89f60dcee162c3953835d565b0728
jamesworden <jamesworden.ny@gmail.com>
2023-09-06 23:27:32 -0400

    Got theme color to change dynamically

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
+import { HeadProps } from 'gatsby';
+import { WEBKIT_THEME_COLOR } from '../constants';
 
-export const Head = () => (
+export const Head: React.FC<HeadProps> = ({}) => (
 	<>
 		<title>James Worden</title>
 		<meta name='description' content="James Worden's portfolio as a software engineer." />
 		<meta name='viewport' content='initial-scale=1, viewport-fit=cover'></meta>
+		<meta name='theme-color' content={WEBKIT_THEME_COLOR.Default} />
 	</>
 );
