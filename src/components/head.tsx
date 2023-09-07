20e19738ba8e4e94c1c41d6a2688c6f9656ee905
jamesworden <jamesworden.ny@gmail.com>
2023-09-06 21:51:54 -0400

    Remove peek div in favor of future webkit safe area

--- src/components/head.tsx
+++ src/components/head.tsx
@@ -4,6 +4,6 @@ export const Head = () => (
 	<>
 		<title>James Worden</title>
 		<meta name='description' content="James Worden's portfolio as a software engineer." />
-		<meta name='viewport' content='width=device-width, initial-scale=1.0'></meta>
+		<meta name='viewport' content='initial-scale=1, viewport-fit=cover'></meta>
 	</>
 );
