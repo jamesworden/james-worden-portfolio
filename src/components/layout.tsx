feac7cbbe4adcc3a6dbf058065c4ca331ecfa094
jamesworden <jamesworden.ny@gmail.com>
2023-10-09 17:19:53 -0400

    Overflow when hamburger menu enabled

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -3,17 +3,17 @@ import { ReactElement } from 'react';
 import { Navbar } from './navbar/navbar';
 import { PageContentProps } from './page-content';
 import { navbarLinks } from '../data/navbar-links/navbar-links';
+import { Overlay } from './overlay';
 
 export interface LayoutProps {
 	children: ReactElement<PageContentProps>;
 	currentPath: string;
 }
 
-export const Layout: React.FC<LayoutProps> = ({ children, currentPath }) => {
-	return (
-		<main className='bg-gray-100 dark:bg-gray-900 transition dark:text-gray-100 h-full min-h-screen justify-between flex flex-col'>
-			<Navbar currentPath={currentPath} navbarLinks={navbarLinks}></Navbar>
-			{children}
-		</main>
-	);
-};
+export const Layout: React.FC<LayoutProps> = ({ children, currentPath }) => (
+	<main className='bg-gray-100 dark:bg-gray-900 transition dark:text-gray-100 h-full min-h-screen justify-between flex flex-col'>
+		<Navbar currentPath={currentPath} navbarLinks={navbarLinks}></Navbar>
+		<Overlay />
+		{children}
+	</main>
+);
