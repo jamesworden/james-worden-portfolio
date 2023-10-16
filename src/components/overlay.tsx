88f53a17527b66aacad2783491267286f6896770
jamesworden <jamesworden.ny@gmail.com>
2023-10-16 08:05:52 -0400

    jwp-101: clicking on overlay should close the hambuger menu

--- src/components/overlay.tsx
+++ src/components/overlay.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
-import { useHamburgerMenu } from '../contexts/hamburger-menu-context';
+import { useHamburgerMenu, useHamburgerMenuUpdate } from '../contexts/hamburger-menu-context';
 
 export const Overlay = () => {
 	const hamburgerMenu = useHamburgerMenu();
+	const toggleHamburgerMenu = useHamburgerMenuUpdate();
 
 	return (
 		<AnimatePresence>
@@ -12,7 +13,8 @@ export const Overlay = () => {
 					initial={{ opacity: 0 }}
 					animate={{ opacity: 0.5 }}
 					exit={{ opacity: 0 }}
-					className='fixed top-0 left-0 right-0 bottom-0 bg-gray-950 z-20'
+					className='fixed top-0 left-0 right-0 bottom-0 bg-gray-950 z-20 cursor-pointer'
+					onClick={() => toggleHamburgerMenu()}
 				></motion.div>
 			)}
 		</AnimatePresence>
